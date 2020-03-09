b8c66ea7551e8e4211712174200bdbb988e58cf9
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-03-09 14:07:09 +0200

    Create lessin8.2sourse.cpp

---
+++ lessin8.2sourse.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+void print_path(int i) {
+	std::cout << "Hello" << std::endl;
+}
+
+static double calc(int a, int b) { //обьявили что у калк внутрення линковка (потому что она статик)
+	
+}
